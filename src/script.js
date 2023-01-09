2fe8b0a911554feb8318ec7df7bfa6e72b894b7f
Franco Meneses <meneses773@gmail.com>
2023-01-08 21:56:27 -0600

    Deleting comments

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 function handleClick() {
   let ulTasks = document.getElementById("id-ul-tasks")
   let newli = document.createElement('li')
-  // newli.appendChild(document.createTextNode("Element " + (le+1)))
   let ids = ulTasks.children.length + 1
   let html = `<div class="tasks-container"> ` +
     `<div class="radiobtn"> ` +
@@ -19,14 +18,12 @@ function handleClick() {
   newli.innerHTML = html
   newli.setAttribute('id', 'li-' + ids)
   ulTasks.appendChild(newli)
-  //console.log(ulTasks.parentNode)
 }
 
 function handleDelete(el) {
   let id = 'li-' + el.id.slice(2)
   let litoDelete = document.getElementById(id)
   litoDelete.remove()
-  // console.log(litodelete)
 }
 
 function handleSave(el) {
