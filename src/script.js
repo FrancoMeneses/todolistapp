7fadc875778015f8e7dd584975ada7b45d034d21
Franco Meneses <meneses773@gmail.com>
2023-01-08 21:54:15 -0600

    Latest changes

--- src/script.js
+++ src/script.js
@@ -5,11 +5,11 @@ function handleClick() {
   let ids = ulTasks.children.length + 1
   let html = `<div class="tasks-container"> ` +
     `<div class="radiobtn"> ` +
-    `<input type="checkbox" id=${'check-' + ids}> ` +
+    `<input type="checkbox" id=${'check-' + ids} onchange="handleCheck(${ids})"> ` +
     `</div> ` +
     `<div class="line"></div> ` +
     `<div class="task" id=${'task-' + ids}> ` +
-    `<input placeholder="Write a new task..." id=${'input-' + ids} onchange="handleChange(event)"> ` +
+    `<input placeholder="Write a new task..." id=${'input-' + ids} onchange=""> ` +
     `</div> ` +
     `<div class="save-delete"> ` +
     `<button class="save" id=${'s-' + ids} onclick="handleSave(this)" >Save</button> ` +
@@ -30,15 +30,64 @@ function handleDelete(el) {
 }
 
 function handleSave(el) {
-  let idInput = 'input-' + el.id.slice(2)
-  let idli = 'li-' + el.id.slice(2)
-  let idButtonS = 's-' + el.id.slice(2)
-  let intoSave = document.getElementById(idInput)
-  document.getElementById(idButtonS).innerText = 'Edit'
-  document.getElementById(idButtonS).className = 'edit'
+  let classN = el.className
+  if (classN === 'save') {
+    let input = document.getElementById('input-' + el.id.slice(2))
+    if (input.value.length !== 0) {
+      let idButtonS = 's-' + el.id.slice(2)
+      let tasktoSave = document.getElementById('task-' + el.id.slice(2))
+
+      document.getElementById(idButtonS).innerText = 'Edit'
+      document.getElementById(idButtonS).className = 'edit'
+      document.getElementById(idButtonS).id = `e-${el.id.slice(2)}`
+
+      let taskText = input.value
+      tasktoSave.removeChild(input)
+      var text = document.createElement('p')
+      text.innerText = taskText
+      text.className = 'doneInput'
+      text.id = `p-${el.id.slice(2)}`
+      tasktoSave.appendChild(text)
+    } else {
+      alert("Tasks can't be empty")
+      document.getElementById(`check-${el.id.slice(2)}`).checked = ""
+    }
+  }
+  if (classN === 'edit') {
+    let p = document.getElementById(`p-${el.id.slice(2)}`)
+    let tasktoEdit = document.getElementById('task-' + el.id.slice(2))
+    let taskText = p.textContent
+    tasktoEdit.removeChild(p)
+    var input = document.createElement('input')
+    input.value = taskText
+    input.className = 'doneInput'
+    input.id = `input-${el.id.slice(2)}`
+    tasktoEdit.appendChild(input)
+
+    let idButtonE = 'e-' + el.id.slice(2)
+    document.getElementById(idButtonE).innerText = 'Save'
+    document.getElementById(idButtonE).className = 'save'
+    document.getElementById(idButtonE).id = `s-${el.id.slice(2)}`
+    input.focus()
+  }
 }
 
-function handleChange(e){
-  let value = e.target.value
-  console.log(e.currentTarget.id)
+function handleCheck(id) {
+  if (document.getElementById(`check-${id}`).checked === true){
+    let button = document.getElementById(`s-${id}`)
+    if (button === null) {
+      button = document.getElementById(`e-${id}`)
+      let p = document.getElementById(`p-${id}`)
+      p.style.textDecoration = 'line-through'
+    } else {
+      handleSave(button)
+      if (document.getElementById(`check-${id}`).checked === true) {
+        let p = document.getElementById(`p-${id}`)
+        p.style.textDecoration = 'line-through'
+      }
+    }
+  }else{
+    let p = document.getElementById(`p-${id}`)
+      p.style.textDecoration = 'none'
+  }
 }
